ba19f8b01e1a4e3d2948f5c1c8e91103f988d47c
Shalom John <shaliz.jhay@gmail.com>
2022-11-29 15:24:58 +0100

    now navigates btw home and own

--- main/views.py
+++ main/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/home.html', {})
+    return render(request, 'main/home.html')
+
+def own(request):
+    return render(request, 'main/own.html')
